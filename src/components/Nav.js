0061790111c241b58ef6c727a7bc6c1512548608
Shreyash Joshi <90604516+Godfathxx@users.noreply.github.com>
2023-11-21 22:32:44 +0530

    Update Nav.js

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,12 +2,10 @@ import {Link} from "react-router-dom";
 function Nav()
 {
     return(
-        <nav class="navbar bg-warning">
-            <Link style={{fontFamily:"Agbalumo"}} to="/" class="navbar-brand mx-3">CRUD operation</Link>
-            <div class="nav">
-                <Link to="/create-Pet" class="nav-link">Create Consumer</Link>
-                <Link to="/Pet-list" class="nav-link">Consumer List</Link>
-            </div>
+        <nav className="navbar" style={{ backgroundColor: "black" }}>
+            <Link style={{fontFamily:"Agbalumo"}} to="/" class="navbar-brand mx-3"><a>
+          <a style={{ Color: "orange" }}>PET </a>Lovers
+        </a></Link>
         </nav>
     )
 }
